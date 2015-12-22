e988ce8280eb8cd3b78cd601f4c3a940ed5321fa
Yobi Myszkowski <tablet.android.lark@gmail.com>
2015-12-22 14:27:59 +0100

    Updating language libraries

--- PolishLanguage/Class1.cs
+++ PolishLanguage/Class1.cs
@@ -11,5 +11,12 @@
         public string Finished { get { return "Zakończone"; } }
         public string Abandoned { get { return "Porzucone"; } }
         public string WebBrowserSucces { get { return "Udało się! Mija Reader jest teraz połączona z Twoim kontem Dropbox."; } }
+
+        public string Login { get { return "Zaloguj się"; } }
+        public string Home { get { return "Dom"; } }
+        public string Library { get { return "Biblioteka"; } }
+        public string Chapters { get { return "Rozdziały"; } }
+        public string Reader { get { return "Czytnik"; } }
+        public string Settings { get { return "Ustawienia"; } }
     }
 }
